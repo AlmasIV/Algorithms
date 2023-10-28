3f915164751d426b83fb1861a39b4e1ab006f476
AlmasIV <Almas.Kairbek.ak@gmail.com>
2023-10-28 13:06:10 +0600

    Added some tests.

--- Selection Sort/Program.cs
+++ Selection Sort/Program.cs
@@ -1,28 +1,73 @@
-﻿namespace Selection_Sort;
+﻿using System.Diagnostics;
+
+namespace Selection_Sort;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Person[] people = new Person[]{
-            new Person("Doe", 21),
-            new Person("John", 21),
-            new Person("Alex", 26),
-            new Person("Jacob", 77),
-            new Person("Andrew", 12),
-            new Person("Micheal", 4),
-            new Person("Zak", 28)
-        };
+        // Person[] people = new Person[]{
+        //     new Person("Doe", 21),
+        //     new Person("John", 21),
+        //     new Person("Alex", 26),
+        //     new Person("Jacob", 77),
+        //     new Person("Andrew", 12),
+        //     new Person("Micheal", 4),
+        //     new Person("Zak", 28)
+        // };
 
-        Console.WriteLine("The original order: ");
-        for(int i = 0; i < people.Length; i ++){
-            Console.WriteLine(i + ". " + people[i] + ". ");
+        // Console.WriteLine("The original order: ");
+        // for(int i = 0; i < people.Length; i ++){
+        //     Console.WriteLine(i + ". " + people[i] + ". ");
+        // }
+        // Console.WriteLine("*** Sorting using selection sort. ***");
+        // Console.WriteLine("After the selection sort, the order became: ");
+        // SelectionSort.Sort(people);
+        // for(int i = 0; i < people.Length; i ++){
+        //     Console.WriteLine(i + ". " + people[i]);
+        // }
+        Random random = new Random();
+        if(!File.Exists("../NumbersToSort.txt")){
+            using(var file = File.OpenWrite("../NumbersToSort.txt")){
+                using(StreamWriter write = new StreamWriter(file)){
+                    for(int i = 0; i < 100000; i ++){
+                        write.WriteLine(random.Next(int.MinValue, int.MaxValue));
+                    }
+                }
+            }
         }
-        Console.WriteLine("*** Sorting using selection sort. ***");
-        Console.WriteLine("After the selection sort, the order became: ");
-        SelectionSort.Sort(people);
-        for(int i = 0; i < people.Length; i ++){
-            Console.WriteLine(i + ". " + people[i]);
+        List<int> data = new List<int>();
+        string[] rawData;
+        using(var file = File.OpenRead("../NumbersToSort.txt")){
+            using(StreamReader reader = new StreamReader(file)){
+                rawData = reader.ReadToEnd().Split('\n');
+                foreach(string str in rawData){
+                    if(int.TryParse(str, out int val)){
+                        data.Add(val);
+                    }
+                }
+            }
         }
+        Console.WriteLine("Here's the unsorted array: ");
+        int indexer = 0;
+        foreach(var num in data){
+            Console.WriteLine(indexer + ". " + num);
+            indexer ++;
+        }
+        Console.WriteLine("Press enter to start the ordering process.");
+        Console.ReadLine();
+        Console.WriteLine("Here's the ordered data: ");
+        Stopwatch stopwatch = new Stopwatch();
+        int[] result = data.ToArray();
+        stopwatch.Start();
+        SelectionSort.Sort<int>(result);
+        stopwatch.Stop();
+        Console.WriteLine("Here's the sorted array: ");
+        indexer = 0;
+        foreach(var num in result){
+            Console.WriteLine(indexer + ". " + num);
+            indexer ++;
+        }
+        Console.WriteLine("Selection Sort took: " + stopwatch.ElapsedMilliseconds + "ms, or " + (stopwatch.ElapsedMilliseconds / 1000) + "s.");
     }
 }
