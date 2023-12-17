3a24dcfb65d15dd4774e1d43f98694c80bb6e265
AlmasIV <Almas.Kairbek.ak@gmail.com>
2023-12-17 11:40:46 +0600

    Added some changes.

---
+++ Quicksort/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Quicksort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = {
+            1, -1, 2, 3, 44, 7
+        };
+
+        Quick<int>.Sort(nums);
+
+        foreach(var v in nums){
+            System.Console.WriteLine(v);
+        }
+    }
+}
---
+++ Quicksort/Quicksort.cs
@@ -0,0 +1,42 @@
+namespace Quicksort;
+
+public class Quick<T> where T : IComparable<T>{
+    public static void Sort(T[] inputs){
+        s_Sort(inputs, 0, inputs.Length - 1);
+    }
+    private static void s_Sort(T[] inputs, int low, int high){
+        if(high <= low){
+            return;
+        }
+        int j = s_Partition(inputs, low, high);
+        s_Sort(inputs, low, j - 1);
+        s_Sort(inputs, j + 1, high);
+    }
+    private static int s_Partition(T[] inputs, int low, int high){
+        int i = low, j = high + 1;
+        T pivot = inputs[low];
+        T temp;
+        while(true){
+            while(inputs[++i].CompareTo(pivot) == -1){
+                if(i == high){
+                    break;
+                }
+            }
+            while(pivot.CompareTo(inputs[--j]) == -1){
+                if(i == low){
+                    break;
+                }
+            }
+            if(i >= j){
+                break;
+            }
+            temp = inputs[i];
+            inputs[i] = inputs[j];
+            inputs[j] = temp;
+        }
+        temp = inputs[low];
+        inputs[low] = inputs[j];
+        inputs[j] = temp;
+        return j;
+    }
+}
