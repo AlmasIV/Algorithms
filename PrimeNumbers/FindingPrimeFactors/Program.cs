2003af284c59e63a7d06ae471b8d5855a8dad497
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-03-05 19:39:41 +0500

    Improved console input / output.

--- PrimeNumbers/FindingPrimeFactors/Program.cs
+++ PrimeNumbers/FindingPrimeFactors/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string? possibleNumber;
+        bool isContinue = true;
+        while(isContinue){
+            Console.WriteLine("Enter a positive number: ");
+            possibleNumber = Console.ReadLine();
+            if(possibleNumber is not null && int.TryParse(possibleNumber, out int number)){
+
+                Console.WriteLine(
+                    PrimeFactor.FindFactors(number)
+                        .Select(prime => prime.ToString())
+                        .Aggregate((str1, str2) => str1 + " * " + str2)
+                );
+                
+            }
+        }
     }
 }
